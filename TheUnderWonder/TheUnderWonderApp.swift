c8f22c2e00c18e1e7b9d31eef18ca37d0ece0598
Dmitry Ihnatiev <straenger@gmail.com>
2021-04-29 11:18:10 +0300

    Initial Commit

---
+++ TheUnderWonder/TheUnderWonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheUnderWonderApp.swift
+//  TheUnderWonder
+//
+//  Created by Дмитрий Игнатьев on 29.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct TheUnderWonderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
